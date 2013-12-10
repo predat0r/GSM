244bc23b41b069d9e2bb0e17c9afad33bbbc3f19
predat0r <alfa.clan@gmail.com>
2013-12-10 17:52:05 +0400

    Change workspace

--- print.py
+++ print.py
@@ -1,12 +1,18 @@
 import SER
-import SER2
+import GPIO
+import sys
 
 SER.set_speed('115200','8N1')
-SER.send('test'+'r')
-SER2.set_speed('115200','8N1')
-SER2.send('test2'+'r')
-print 'test'
-SER.send('test1')
-SER.sendbyte(0x0d, 0)
-SER2.send('test2')
-SER2.sendbyte(0x0d, 0)
+GPIO.setIOvalue(7,1)
+
+class SerWriter:
+    def __init__(self):
+        SER.set_speed('115200','8N1')
+
+    def write(self,s):
+        SER.send(s+'r')
+
+sys.stdout = sys.stderr = SerWriter()
+GPIO.setIOvalue(7,0)
+
+print 'Test UART\r'
